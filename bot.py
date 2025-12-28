4fed0f680f07cda1e719ca35501d149e0d3eac2a
eathanhu <eathanhunt2023@gmail.com>
2025-12-28 11:49:21 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,40 @@
+import os
+from pyrogram import Client, filters
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHANNEL_TAG = os.environ.get("CHANNEL_TAG")
+
+app = Client(
+    "caption_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+@app.on_message(filters.channel & ~filters.text)
+async def add_caption(client, message):
+
+    if not (
+        message.document or message.video or message.audio or
+        message.voice or message.photo
+    ):
+        return
+
+    old_caption = message.caption or ""
+
+    if CHANNEL_TAG in old_caption:
+        return
+
+    new_caption = (
+        f"{old_caption}\n\n{CHANNEL_TAG}"
+        if old_caption else CHANNEL_TAG
+    )
+
+    try:
+        await message.edit_caption(new_caption)
+    except:
+        pass
+
+app.run()
